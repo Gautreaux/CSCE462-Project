d442e8885610b723aafb4802165505e778ea1efa
Spencer 462Pi <sgautreaux99@gmail.com>
2020-11-05 21:36:35 +0000

    Adhoc work

--- adhoc/i2cTesting.py
+++ adhoc/i2cTesting.py
@@ -2,22 +2,85 @@ import board
 import busio
 
 import time
+import RPi.GPIO as GPIO
+import threading
 
-from digitalio import Direction
+from digitalio import Direction, Pull
 from adafruit_mcp230xx.mcp23017 import MCP23017
 i2c = busio.I2C(board.SCL, board.SDA)
 
 mcp = MCP23017(i2c)
+pinR = mcp.get_pin(7)
+pinG = mcp.get_pin(6)
+pinB = mcp.get_pin(5)
 
-pin0 = mcp.get_pin(7)
-pin0.direction = Direction.OUTPUT
-pin0.switch_to_output(value=True)
+pinL = [pinR, pinG, pinB]
 
-pin0.value = True
+for pin in pinL:
+    pin.direction = Direction.OUTPUT
+    pin.switch_to_output(value=True)
+    pin.value=False
+threadRunning = True
+def rbgledThread():
+    
 
-while True:
-    print("HIGH")
-    pin0.value = True
-    time.sleep(.5)
-    pin0.value = False
-    time.sleep(.5)
+    def stateGenerator(period):
+        s1 = 0
+        s2 = 0
+        s3 = 0
+
+        while threadRunning:
+            if s1 is 0:
+                s1 = 1
+            else:
+                s1 = 0
+                if s2 is 0:
+                    s2 = 1
+                else:
+                    s2 = 0
+                    if s3 is 0:
+                        s3 = 1
+                    else:
+                        s3 = 0
+            time.sleep(period)
+            yield [s3, s2, s1]
+        return
+
+    try:
+        for state in stateGenerator(.5):
+            pinR.value = (state[0] is 1)
+            pinB.value = (state[1] is 1)
+            pinG.value = (state[2] is 1)
+    finally:
+        pinR.value = False
+        pinB.value = False
+        pinG.value = False
+
+
+def interruptCallback(_):
+    print("Callback")
+
+try:
+    t = threading.Thread(target=rbgledThread)
+    t.start()
+
+    pinButton = mcp.get_pin(8)
+    # pinButton.direction = Direction.OUTPUT
+    # pinButton.switch_to_output(value=True)
+    # pinButton.value=True
+    pinButton.direction = Direction.INPUT
+    pinButton.pull = Pull.UP
+    lastButtonState = False
+
+    while True:
+        v = pinButton.value
+        if v != lastButtonState:
+            print("Pressed" if not v else "Released")
+            lastButtonState = v
+        time.sleep(.05)
+
+finally:
+    threadRunning = False
+    pinR.value = False
+    pinB.value = False
+    pinG.value = False
